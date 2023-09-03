65554285ac50f7863b755cc0325521add983adfe
Ben Keys <bkeys818@gmail.com>
2023-09-03 17:34:57 -0400

    Create new test testDidGuess42Correctly

--- src/test/java/edu/bsu/cs222/SillyMethodsTest.java
+++ src/test/java/edu/bsu/cs222/SillyMethodsTest.java
@@ -1,3 +1,13 @@
 package edu.bsu.cs222;
 
-public class SillyMethodsTest {}
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class SillyMethodsTest {
+    @Test
+    public void testDidGuess42Correctly(){
+        SillyMethods sillyMethods = new SillyMethods();
+        boolean result = sillyMethods.didGuess42(42);
+        Assertions.assertTrue(result);
+    }
+}
