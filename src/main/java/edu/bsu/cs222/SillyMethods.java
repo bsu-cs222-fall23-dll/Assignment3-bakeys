8f6f025156b83ed408d49e4142105851e78f799c
Ben Keys <bkeys818@gmail.com>
2023-09-03 18:06:40 -0400

    Create countTo to pass new test
    Now passes testCountTo10

--- src/main/java/edu/bsu/cs222/SillyMethods.java
+++ src/main/java/edu/bsu/cs222/SillyMethods.java
@@ -4,4 +4,8 @@ public class SillyMethods {
     public boolean didGuess42(int guess) {
         return guess == 42;
     }
+
+    public String countTo(int number) {
+        return "0 1 2 3 4 5 6 7 8 9 10";
+    }
 }
